57bde2439c33e362f63073321787ed57f2f7476a
Denis Olivares <denis.os@gmail.com>
2020-11-11 13:56:59 -0300

    infra completa - teste bloqueado por causa da limitacao da oci

--- variables.tf
+++ variables.tf
@@ -4,18 +4,17 @@ variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
-//variable "public_key_oci" {}
+variable "public_key_oci" {}
 //variable "private_key_oci" {}
 
-# SDDC NAME
-variable sddc_name {
-    description =   "Variavel para identificar o SDDC"
-    default =   "yoyoci"
+variable sddc_display_name {
+    description =   "(Optional) (Updatable) A descriptive name for the SDDC. SDDC name requirements are 1-16 character length limit, Must start with a letter, Must be English letters, numbers, - only, No repeating hyphens, Must be unique within the region. Avoid entering confidential information"
+    default =   "test_sddc_vmware"
 }
 
-/* variable sddc_name {
-    description =   "Variavel para identificar o SDDC"
-    default =   "yoyoci"
+/* variable sddc_esxi_hosts_count {
+    description =   "(Required) The number of ESXi hosts to create in the SDDC. You can add more hosts later - Minimum 3 required, maximum 64"
+    default =   "3"
 } */
 
 
